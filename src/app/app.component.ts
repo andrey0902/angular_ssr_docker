d730c8314c4c46f0feb9fe4844c5581b5fb41a71
andrey0902 <andreypavlov1706@gmail.com>
2019-08-06 22:44:20 +0300

    test deploy 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-ssr-cd';
+  title = 'angular-ssr-cd for test with deploy';
 }

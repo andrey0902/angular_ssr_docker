788da557ef04fc8aa1453242058a5ad0b63aa8a0
andrey0902 <andreypavlov1706@gmail.com>
2019-08-06 22:17:57 +0300

    test deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
 set -x
-chmod 777 ./dist/server.js;
-echo "ls ./dist/server.js -la";
-tar -czf package.tgz ./dist/travis;
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -P 54231 package.tgz usr01@188.166.54.169:/var/www/light-it-08.tk/;
-ssh -o "StrictHostKeyChecking=no" -p"$PORT" "$REMOTEUSER"@"$REMOTEHOST" "rm -rf /var/www/light-it-08.tk/*";
-scp -P"$PORT" package.tgz "$REMOTEUSER"@"$REMOTEHOST":/var/www/light-it-08.tk/;
-ssh -p"$PORT" "$REMOTEUSER"@"$REMOTEHOST" "cd /var/www/light-it-08.tk/ && tar -xvf /var/www/light-it-08.tk/package.tgz";
+ssh -o "StrictHostKeyChecking=no" -p54231 usr01@188.166.54.169 ""
+CHECK_ANGULAR=$(ssh -o "StrictHostKeyChecking=no" -p54231 usr01@188.166.54.169 "docker ps -a --format \"{{.Names}}\" | grep angular_ssr")
+if [[ "$CHECK_ANGULAR" == "angular_ssr_1" ]]
+then
+  ssh -o "StrictHostKeyChecking=no" -p54231 usr01@188.166.54.169 "docker rm -f angular_ssr_1 && docker rmi -f 6633/angular-ssr:angular_ssr"
+  ssh -o "StrictHostKeyChecking=no" -p54231 usr01@188.166.54.169 "docker login -u 6633 -p qwerty123 && docker run --name angular_ssr_1 -p 4202:4000 6633/angular-ssr:angular_ssr"
+  else
+  ssh -o "StrictHostKeyChecking=no" -p54231 usr01@188.166.54.169 "docker login -u 6633 -p qwerty123 && docker run --name angular_ssr_1 -p 4202:4000 6633/angular-ssr:angular_ssr"
+fi
--- scripts/push.sh
+++ scripts/push.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push "$DOCKER_USERNAME"/"$DOCKER_REPO":angular_ssr
-
